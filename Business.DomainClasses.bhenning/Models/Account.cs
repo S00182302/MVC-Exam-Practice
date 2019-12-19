2f3971835e6d2ef5f45d5252946a74aee0122e98
s00182302@mail.itsligo.ie <s00182302@mail.itsligo.ie>
2019-12-19 11:18:43 +0000

    Initial commit 2

---
+++ Business.DomainClasses.bhenning/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DomainClasses.bhenning.Models
+{
+    class Account
+    {
+        [Key]
+        public int ID { get; set; }
+        public String AccountName { get; set; }
+        public DateTime InceptionDate { get; set; }
+        [ForeignKey("associatedCustomer")]
+        public int CustomerID { get; set; }
+        [Column(TypeName = "Money")]
+        public decimal CurrentBalance { get; set; }
+        public int AccountTypeID { get; set; }
+
+        public virtual Customer associatedCustomer { get; set; }
+    }
+}
---
+++ MVCbhenning/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCbhenning.MvcApplication" Language="C#" %>
